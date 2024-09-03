5e11e311f192158bfa9ec5f1d74b3dcca6e33c91
syomazh <108838306+syomazh@users.noreply.github.com>
2024-09-03 16:10:18 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// team 1
+function button1_clicked() {
+    //write what u want to happen when the button is clicked
+}
+document.getElementById('button1').addEventListener('click', button1_clicked);
+
+
+
+
+// team 2
+function button2_clicked() {
+    //write what u want to happen when the button is clicked
+}
+document.getElementById('button2').addEventListener('click', button2_clicked);
+
+
+
+
+// team 1
+function button3_clicked() {
+    //write what u want to happen when the button is clicked
+}
+document.getElementById('button1').addEventListener('click', button3_clicked);
