f5f1332cc753c1bb9ff850f1c2ab4b429d0f2805
nuts-iko <nutsikosa@gmail.com>
2024-09-03 19:15:21 -0500

    orang!!!!!!
    orang.

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ document.getElementById('button1').addEventListener('click', button1_clicked);
 
 // team 2
 function button2_clicked() {
-    //write what u want to happen when the button is clicked
+    //Good job clickn yo
+    document.getElementById("button2").style.background='#ffa500';
+    button2.textContent = "lesgooo bobby's boppn branch is winnen dis ting";
+    button2.style.width = '2000px'; // setting the width to 200px
+    button2.style.height = '2000px'; // setting the height to 200px
+    button2.style.fontSize = '200px'; // setting the font size to 20px
+    button2.style.color = '#ff007f'; // setting the color to white
+    var img = document.createElement("img");
+    img.src = "/Users/nutsa/Documents/orang.webp";
+    var src = document.getElementById("header");
+    src.appendChild(img);
+
 }
 document.getElementById('button2').addEventListener('click', button2_clicked);
 
